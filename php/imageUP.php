253eabe38e07c87e6e9a53f655ae16cb176dba13
Dan Jones <dan_j@live.co.uk>
2015-10-24 15:42:53 +0100

    file uploadwee

--- php/imageUP.php
+++ php/imageUP.php
@@ -1,19 +1,18 @@
 <?php
-
- header("Access-Control-Allow-Origin: *");
-$ds          = "/";  //1
- 
-$storeFolder = 'images';   //2
- 
-if (!empty($_FILES)) {
-     
-    $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
- 
-    move_uploaded_file($tempFile,$targetFile); //6
-     
-}    
+ //header("Access-Control-Allow-Origin: *");
+$target_dir = "images/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
 ?>
