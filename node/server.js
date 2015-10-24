7949ced35fb49fc85a28836c8e387e33132a75ce
Misha Wagner <mishajw@gmail.com>
2015-10-24 17:50:42 +0100

    Some styling

--- node/server.js
+++ node/server.js
@@ -2,6 +2,8 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log(request);
+    
   response.write("It's alive!");
   response.end();
 }).listen(3000);
