5662b284bb078a32f69c10c3fcbbdc8a685efa3f
Dan Jones <dan_j@live.co.uk>
2015-10-24 18:14:20 +0100

    Fixed upload + getting data

--- node/server.js
+++ node/server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var multer = require('multer');
-var upload = multer({ dest: "uploads/" });
+var crypto = require('crypto');
+var path = require('path');
+
+var storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+})
+
+var upload = multer({ storage: storage })
+
+
 var app = express();
 
 
@@ -16,7 +32,7 @@ app.get('/misha', function (req, res) {
 });
 
 app.post('/upload', upload.single("file"), function (req, res) {
-  console.log(req.files);
+  console.log(req.file);
 });
 
 var server = app.listen(3000, function () {
