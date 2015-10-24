9b2a9241526b69ec77e3a873bbb84e3b71b72de0
EC2 Default User <ec2-user@ip-172-31-16-154.eu-west-1.compute.internal>
2015-10-24 14:33:42 +0000

    merge

--- js/drop.js
+++ js/drop.js
@@ -1,79 +1,109 @@
-var template = '<div class="preview">' + 
-               '<span class="imageHolder">' +
-               '<img />' +
-               '<span class="uploaded"></span>' +
-               '<div class="progressHolder">' +
-               '<div class="progress"></div>' +
-               '</div>'+
-               '<div>';
+console.log("abc");
+
+var template = '<div class="preview">' +
+    '<span class="imageHolder">' +
+    '<img />' +
+    '<span class="uploaded"></span>' +
+    '</span>' +
+    '<div class="progressHolder">' +
+    '<div class="progress"></div>' +
+    '</div>' +
+    '</div>';
 
 function createImage(file) {
-  var preview = $(template), image = $('img', preview);
 
-  var reader = new FileReader();
+    var preview = $(template),
+        image = $('img', preview);
 
-  image.width = 100;
-  image.height = 100;
+    var reader = new FileReader();
 
-  reader.onload = function(e ) {
-    image.attr('src',e.target.result);
-  };
+    image.width = 100;
+    image.height = 100;
 
-  reader.readAsDataURL(file);
+    reader.onload = function(e) {
 
-  message.hide();
-  preview.appendTo(upload-box);
+        // e.target.result holds the DataURL which
+        // can be used as a source of the image:
 
-  $.data(file,preview);
+        image.attr('src', e.target.result);
+    };
+
+    // Reading the file as a DataURL. When finished,
+    // this will trigger the onload function above:
+    reader.readAsDataURL(file);
+
+    message.hide();
+    preview.appendTo(dropbox);
+
+    // Associating a preview container
+    // with the file, using jQuery's $.data():
+
+    $.data(file, preview);
 }
 
 $(function(){
-  var uploadbox = $('#upload-box)'), message = $('.message', uploadbox);
-  uploadbox.filedrop ({
-    paramname: 'pic',
-    maxfiles: 20,
-    maxfilesize: 10,
-    url: 'http://ec2-52-16-199-4.eu-west-1.compute.amazonaws.com/php/imageUP.php',
-    uploadFinished:function(i,file,response) {
-      $.data(file).addClass('done');
-    },
-    error: function(err, file) {
-      switch(err) {
-        case 'BrowserNotSupported':
-          showMessage('Your browser does not support HTML5 file uploads...');
-          break;
-        case 'TooManyFiles':
-          alert('You have tried to upload too many files');
-          break;
-        case 'FileTooLarge':
-          alert(file.name + ' is too large.');
-          break;
-        default:
-          break;
-      },
-    },
-    // called before each upload
-    beforeEach: function(file){
-      if(!file.type.match(/^image\//)){
-        alert('Only images can be uploaded.';)
-        return false;
-      }
-    },
 
-    uploadStarted: function(i, file, len){
-      createImage(file);
-    },
-    
-      progressUpdated: function(i, file, progress){
-        $.data(file).find('.progress').width(progress);
-      }
-    });
-  var template = '...'
+	var dropbox = $('#dropbox'),
+		message = $('.message', dropbox);
 
-  function showMessage(msg){
-    message.html(msg);
-  }
+	dropbox.filedrop({
+		// The name of the $_FILES entry:
+		paramname:'pic',
+
+		maxfiles: 5,
+    	maxfilesize: 2, // in mb
+		url: 'post_file.php',
+
+		uploadFinished:function(i,file,response){
+			$.data(file).addClass('done');
+			// response is the JSON object that post_file.php returns
+		},
+
+    	error: function(err, file) {
+			switch(err) {
+				case 'BrowserNotSupported':
+					showMessage('Your browser does not support HTML5 file uploads!');
+					break;
+				case 'TooManyFiles':
+					alert('Too many files! Please select 5 at most!');
+					break;
+				case 'FileTooLarge':
+					alert(file.name+' is too large! Please upload files up to 2mb.');
+					break;
+				default:
+					break;
+			}
+		},
+
+		// Called before each upload is started
+		beforeEach: function(file){
+			if(!file.type.match(/^image\//)){
+				alert('Only images are allowed!');
+
+				// Returning false will cause the
+				// file to be rejected
+				return false;
+			}
+		},
+
+		uploadStarted:function(i, file, len){
+			createImage(file);
+		},
+
+		progressUpdated: function(i, file, progress) {
+			$.data(file).find('.progress').width(progress);
+		}
+
+	});
+
+	var template = '...'; 
+
+	function createImage(file){
+		// ... see above ...
+	}
+
+	function showMessage(msg){
+		message.html(msg);
+	}
+
 });
-
-
-
