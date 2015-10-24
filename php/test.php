9de8a98556557e3a8a658c020e1a42a79c044cc0
Dan Jones <dan_j@live.co.uk>
2015-10-24 13:21:14 +0100

    "testing web stuff"

--- php/test.php
+++ php/test.php
@@ -1,4 +1,22 @@
 <?php
-echo "test";
 
+echo $_SERVER['SERVER_NAME'];
+
+/*)
+   if(isset($_FILES['image'])){
+    $errors= array();
+    $file_name = $_FILES['image']['name'];
+    $file_size =$_FILES['image']['size'];
+    $file_tmp =$_FILES['image']['tmp_name'];
+    $file_type=$_FILES['image']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+    $expensions= array("jpeg","jpg","png");
+      
+    
+      
+    move_uploaded_file($file_tmp,"images/".$file_name);
+   }
+   
+   */
 ?>
