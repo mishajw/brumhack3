c56ac7f84394cb4ed7e9d0f1eea2a1ea70354518
Dan Jones <dan_j@live.co.uk>
2015-10-24 13:39:30 +0100

    Removed failed attempted at auto pulling, and done image uploader

--- php/serverpull.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo shell_exec("cd /var/www/html/brumhack3/ && sudo git pull");
-?>
--- php/test.php
+++ php/test.php
@@ -1,8 +1,6 @@
 <?php
 
-echo $_SERVER['SERVER_NAME'];
 
-/*)
    if(isset($_FILES['image'])){
     $errors= array();
     $file_name = $_FILES['image']['name'];
@@ -10,13 +8,15 @@ echo $_SERVER['SERVER_NAME'];
     $file_tmp =$_FILES['image']['tmp_name'];
     $file_type=$_FILES['image']['type'];
     $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-      
-    $expensions= array("jpeg","jpg","png");
-      
     
       
     move_uploaded_file($file_tmp,"images/".$file_name);
+	
+	echo $_SERVER['SERVER_NAME']."images/".$file_name;
+   }
+   else
+   {
+	   echo "No file supplied!";
    }
    
-   */
 ?>
