27de3b9bb9842ef96d93f0fac31df46b307229b2
Dan Jones <dan_j@live.co.uk>
2015-10-24 15:03:03 +0100

    I'm just trying to get....

--- php/getTags.php
+++ php/getTags.php
@@ -1,3 +1,6 @@
 <?php
-	http_post_fields("https://api.clarifai.com/v1/tag/", '"Authorization: Bearer JsoGCsAUVb1p4ADqHSMJ6EKfUv4WRV" --data-urlencode "url=http://www.clarifai.com/img/metro-north.jpg" --data-urlencode "url=http://www.clarifai.com/img/metro-north.jpg"');
+
+	http_get("https://api.clarifai.com/v1/tag/", array("header"=>"Authorization: Bearer JsoGCsAUVb1p4ADqHSMJ6EKfUv4WRV"), $info)
+	echo $info;
 ?>
+
